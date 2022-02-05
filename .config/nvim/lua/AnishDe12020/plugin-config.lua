9974da213062827751924ebad4f7c70b1d26df78
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-05 17:32:50 +0530

    Added lazygit as a custom terminal which would open on float

--- .config/nvim/lua/AnishDe12020/plugin-config.lua
+++ .config/nvim/lua/AnishDe12020/plugin-config.lua
@@ -78,3 +78,16 @@ let g:startify_session_before_save = [
 -- -- ]
 -- -- ]])
 
+require("toggleterm").setup {
+  open_mapping = [[<C-\>]],
+}
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
