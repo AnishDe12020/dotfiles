e332836add85b1dde50f39e592decc91ea86e242
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-09 14:36:11 +0530

    Add vim-gitgutter

--- .config/nvim/lua/AnishDe12020/plugins.lua
+++ .config/nvim/lua/AnishDe12020/plugins.lua
@@ -53,7 +53,8 @@ return require("packer").startup({
     --   config = function ()
     --     require'alpha'.setup(require'alpha.themes.startify'.config)
     --   end
-    -- } 
+    -- }
+    use 'airblade/vim-gitgutter'
 
     -- tpope
     use 'tpope/vim-surround'
