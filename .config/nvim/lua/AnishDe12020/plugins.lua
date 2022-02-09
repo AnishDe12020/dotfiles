c6a7bdae30321184b6d377c78f669a10d70fd6b6
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-09 15:23:39 +0530

    Switched to coq from cmp

--- .config/nvim/lua/AnishDe12020/plugins.lua
+++ .config/nvim/lua/AnishDe12020/plugins.lua
@@ -69,20 +69,21 @@ return require("packer").startup({
     -- lsp
     use 'neovim/nvim-lspconfig'
     use 'williamboman/nvim-lsp-installer'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
+    -- use 'hrsh7th/cmp-nvim-lsp'
+    -- use 'hrsh7th/cmp-buffer'
+    -- use 'hrsh7th/cmp-path'
+    -- use 'hrsh7th/cmp-cmdline'
+    -- use 'hrsh7th/nvim-cmp'
     -- use 'hrsh7th/nvim-treesitter-langserver'
-    -- use {'ms-jpq/coq_nvim', branch = 'coq'}
-		-- use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
-		-- use {'ms-jpq/coq.thirdparty', branch = '3p'}
+    use {'ms-jpq/coq_nvim', branch = 'coq'}
+		use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
+		use {'ms-jpq/coq.thirdparty', branch = '3p'}
+    use "lithammer/nvim-diagnosticls"
 
     -- luasnip
     use 'L3MON4D3/LuaSnip'
     use 'rafamadriz/friendly-snippets'
-    use 'saadparwaiz1/cmp_luasnip'
+    -- use 'saadparwaiz1/cmp_luasnip'
 
     -- misc
 
