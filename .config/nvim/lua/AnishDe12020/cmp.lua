79d60d3522e64d6970de05b1f799f63b5c01efee
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-19 14:09:49 +0530

    Removed CMP Tabnine sorting

--- .config/nvim/lua/AnishDe12020/cmp.lua
+++ .config/nvim/lua/AnishDe12020/cmp.lua
@@ -119,20 +119,6 @@ cmp.setup({
 		ghost_text = false,
 		native_menu = false,
 	},
-	sorting = {
-		priority_weight = 2,
-		comparators = {
-			require("cmp_tabnine.compare"),
-			compare.offset,
-			compare.exact,
-			compare.score,
-			compare.recently_used,
-			compare.kind,
-			compare.sort_text,
-			compare.length,
-			compare.order,
-		},
-	},
 })
 
 cmp.setup.cmdline("/", {
